080d73774d594cdf257a5a0fa0da445a90a46ab8
Syed Zaki <56019676+syedzaki114689@users.noreply.github.com>
2020-12-16 13:10:04 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+class brick():
+    def __init__(self, pos):
+        self.size = (2, 1)
+        self.gridpos = pos
+
+
+def maiin():
+    rows, cols = (7, 8)
+    arr = [[0]*rows]*cols
+    print(arr)
+
+
+maiin()
